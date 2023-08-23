f3a4427479a9fc58bda14ddcf21a59bb8417e4ea
rain-rain-bob-sss <61649368+rain-rain-bob-sss@users.noreply.github.com>
2023-08-23 15:10:59 +0800

    Update sv_entitieshealth.lua

--- flood/gamemode/server/sv_entitieshealth.lua
+++ flood/gamemode/server/sv_entitieshealth.lua
@@ -5,6 +5,7 @@ local hp={
   ['gmod_thruster']=50
 }
 hook.Add('OnEntityCreated','Entity health',function(ent)
-  ent:SetNWInt("CurrentPropHealth",hp[ent:getClass()] or 0)
-	ent:SetNWInt("BasePropHealth",mhp[ent:getClass()] or 0)
+	if(not IsValid(ent))then return end
+  	ent:SetNWInt("CurrentPropHealth",hp[ent:GetClass()] or 0)
+	ent:SetNWInt("BasePropHealth",mhp[ent:GetClass()] or 0)
 end)

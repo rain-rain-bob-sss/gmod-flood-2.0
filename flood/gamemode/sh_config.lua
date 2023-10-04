7cc891a42724589b5c57e43a38ca481e1b3a9681
[TW]Rain_Bob <61649368+rain-rain-bob-sss@users.noreply.github.com>
2023-10-04 23:05:59 +0800

    Update sh_config.lua

--- flood/gamemode/sh_config.lua
+++ flood/gamemode/sh_config.lua
@@ -1,4 +1,7 @@
 --Unused until mass update
 FM_ShouldOverrideGameDescription=false
-FM_GetGameDescription="Flood 2.2"
-FM_FallDamageType=0
+--FM_GetGameDescription="Flood 2.2"
+--FM_FallDamageType=0
+function GM:GetGameDescription()
+  return "Flood 2.2 By [TW]Rain_bob" and FM_ShouldOverrideGameDescription or ""
+end

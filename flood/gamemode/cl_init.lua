76a5bb1ee90e63d606809c657959ca53ee549030
[TW]Rain_Bob <61649368+rain-rain-bob-sss@users.noreply.github.com>
2023-10-26 17:06:00 +0800

    Update cl_init.lua

--- flood/gamemode/cl_init.lua
+++ flood/gamemode/cl_init.lua
@@ -13,15 +13,24 @@ for _, file in pairs(file.Find("flood/gamemode/client/vgui/*.lua", "LUA")) do
 	MsgN("-> "..file)
 	include("flood/gamemode/client/vgui/"..file)
 end
-
-function GM:SpawnMenuOpen(ply)
+local no=function()
 	return false
 end
-
-function GM:ContextMenuOpen(ply)
-	return false
-end
-
+--function GM:SpawnMenuOpen(ply)
+--	return false
+--end
+--function GM:ContextMenuOpen(ply)
+--	return false
+--end
+GM.SpawnMenuOpen=no
+GM.ContextMenuOpen=no
+hook.Add("Think","No you dumbass",function()
+	if(GM.SpawnMenuOpen~=no)then
+		GM.SpawnMenuOpen=no
+	elseif(GM.ContextMenuOpen~=no)then
+		GM.ContextMenuOpen=no
+	end
+end)
 function GM:CanProperty(ply, property, ent)
 	return false
 end

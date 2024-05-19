e62f4aed902ba48bdda9518f0e3922526166187a
chickencvgmailcom <61649368+rain-rain-bob-sss@users.noreply.github.com>
2024-05-19 14:46:17 +0800

    better

--- gamemodes/flood/gamemode/server/sv_boatowner.lua
+++ gamemodes/flood/gamemode/server/sv_boatowner.lua
@@ -1,18 +1,13 @@
-local function PlayerIsFriend(ply, ply2)
-	if not IsValid(ply) or not IsValid(ply2) then return end
-
-	for k, v in pairs(ply:CPPIGetFriends()) do
-		if v == ply2 then return true end 
-	end
-
-	return false 
+local function NADMODISFRIEND(PLY1,PLY2)
+	local friends = (NADMOD.Users[PLY1:SteamID()] or {Friends={}}).Friends
+	return friends[PLY2:SteamID()]
 end
 
 timer.Create("Flood:OffProps", 0.08, 0, function() 
 	if GAMEMODE:GetGameState() == 2 or GAMEMODE:GetGameState() == 3 then
 		for _, v in ipairs(player.GetAll()) do 
 			local ent = v:GetGroundEntity()
-			if ent and IsValid(ent) and ent:GetClass() == "prop_physics" and ent:CPPIGetOwner() != v and PlayerIsFriend(ent:CPPIGetOwner(), v) == false then 
+			if ent and IsValid(ent) and ent:GetClass() == "prop_physics" and ent:CPPIGetOwner() != v and NADMODISFRIEND(ent:CPPIGetOwner(), v) == false then 
 				local dmginfo=DamageInfo()
 				dmginfo:SetDamage(10)
 				dmginfo:SetAttacker(ent)

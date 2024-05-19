2b0b74f8bf77dfbf7dcb701bd38b353d00e3be32
[TW]Rain_Bob <61649368+rain-rain-bob-sss@users.noreply.github.com>
2024-05-19 13:32:50 +0800

    Create sv_teamchat.lua
    .

---
+++ flood/gamemode/server/sv_teamchat.lua
@@ -0,0 +1,28 @@
+local function PlayerIsFriend(ply, ply2)
+  	if not IsValid(ply) or not IsValid(ply2) then return end
+  
+  	for k, v in pairs(ply:CPPIGetFriends()) do
+  		if v == ply2 then return true end 
+  	end
+  
+  	return false 
+end
+local gr=Color(8,93,55)
+hook.Add("PlayerSay","TeamChatTempFix!",function(ply,txt,team)
+    if(team)then
+        local friends=ply:CPPIGetFriends()
+        local ct=ChatText()
+        ct:AddText("[TEAM]",gr)
+        ct:AddText(ply:Nick(),team.GetColor(ply:Team()))
+        ct:AddText(":")
+        ct:AddText(txt)
+        ct:Send(ply)
+        for _,v in pairs(friends)do
+            if(PlayerIsFriend(v,ply))then
+                ct:Send(v)
+            end
+        end
+        ct=nil collectgarbage("step", 64)
+        return true
+    end 
+end)

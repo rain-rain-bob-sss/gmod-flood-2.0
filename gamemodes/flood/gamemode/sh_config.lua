2860eefa736452afe57493ad4ea645ec9f007694
[TW]Rain_Bob <61649368+rain-rain-bob-sss@users.noreply.github.com>
2025-02-02 10:49:47 +0800

    wat

--- gamemodes/flood/gamemode/sh_config.lua
+++ gamemodes/flood/gamemode/sh_config.lua
@@ -3,5 +3,5 @@ FM_ShouldOverrideGameDescription=false
 FM_GetGameDescription="Flood 2.2"
 --FM_FallDamageType=0
 function GM:GetGameDescription()
-  return FM_GetGameDescription and FM_ShouldOverrideGameDescription or ""
+  return FM_ShouldOverrideGameDescription and FM_GetGameDescription or ""
 end
